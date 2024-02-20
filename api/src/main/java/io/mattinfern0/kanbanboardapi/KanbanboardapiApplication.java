d438051d180c831800caacd3a1e7f1399636db24
mattinfern0 <mattnfernandez@gmail.com>
2024-02-19 16:33:20 -0800

    initial commit

---
+++ api/src/main/java/io/mattinfern0/kanbanboardapi/KanbanboardapiApplication.java
@@ -0,0 +1,13 @@
+package io.mattinfern0.kanbanboardapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KanbanboardapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KanbanboardapiApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/io/mattinfern0/kanbanboardapi/KanbanboardapiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.mattinfern0.kanbanboardapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KanbanboardapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
