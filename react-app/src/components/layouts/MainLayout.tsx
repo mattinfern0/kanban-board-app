7aca0b1f78c7ef19c71076b189e13074e9cfbc24
mattinfern0 <mattnfernandez@gmail.com>
2024-03-10 00:21:33 -0800

    WIP: sample page

---
+++ react-app/src/components/layouts/MainLayout.tsx
@@ -0,0 +1,21 @@
+import {AppBar, Container, Toolbar, Typography} from "@mui/material";
+import React from "react";
+
+interface MainLayoutProps {
+    children: React.ReactNode;
+}
+
+export const MainLayout = ({children}: MainLayoutProps) => {
+    return (
+        <>
+            <AppBar position="static" p>
+                <Toolbar>
+                    <Typography variant="h6">Kanban App</Typography>
+                </Toolbar>
+            </AppBar>
+            <Container>
+                {children}
+            </Container>
+        </>
+    )
+}
---
+++ react-app/src/routes/index.tsx
@@ -0,0 +1,20 @@
+import {RouteObject} from "react-router-dom";
+import {Typography} from "@mui/material";
+import {MainLayout} from "@/components/layouts/MainLayout.tsx";
+
+const SamplePage = () => {
+    return (
+        <Typography>Hello World!</Typography>
+    )
+}
+
+export const routes: RouteObject[] = [
+    {
+        path: "/",
+        element: (
+            <MainLayout>
+                <SamplePage/>
+            </MainLayout>
+        ),
+    },
+];
