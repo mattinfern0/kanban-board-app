c51fd46ee4b4f2fa39e8f23fec86975e5e3316cb
mattinfern0 <mattnfernandez@gmail.com>
2024-12-15 21:28:04 -0800

    REF: hardcode firebase config values

--- react-app/src/features/auth/lib/firebase.ts
+++ react-app/src/features/auth/lib/firebase.ts
@@ -2,12 +2,14 @@ import { initializeApp } from "firebase/app";
 import { connectAuthEmulator, getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  // Firebase docs say it's safe to expose this API key. https://firebase.google.com/docs/projects/api-keys
+  // Instead use Firebase rules to secure the data.
+  apiKey: "AIzaSyBMedGF42YKpqlFg1_dBadpW_oOkRk68C8",
+  authDomain: "kanban-app-b5cd2.firebaseapp.com",
+  projectId: "kanban-app-b5cd2",
+  storageBucket: "kanban-app-b5cd2.firebasestorage.app",
+  messagingSenderId: "667573382391",
+  appId: "1:667573382391:web:ab7c4f01c861ed144af7f2",
 };
 
 const authEmulatorUrl: string | null = import.meta.env.VITE_FIREBASE_AUTH_EMULATOR_URL;
