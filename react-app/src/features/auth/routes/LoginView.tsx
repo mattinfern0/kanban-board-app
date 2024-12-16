83c69f9ba572b90049b32c74f26c19e054e0aa43
mattinfern0 <mattnfernandez@gmail.com>
2024-12-15 20:23:11 -0800

    FEAT: show error message if login failed

--- react-app/src/features/auth/routes/LoginView.tsx
+++ react-app/src/features/auth/routes/LoginView.tsx
@@ -1,8 +1,20 @@
 import { LoginFormValues } from "@/features/auth/types";
-import { Card, Title } from "@mantine/core";
+import { Alert, Card, Title } from "@mantine/core";
 import { LoginForm } from "@/features/auth/components/LoginForm.tsx";
 import { useLoginMutation } from "@/features/auth/api/login.ts";
 import { useNavigate } from "react-router-dom";
+import { FirebaseError } from "firebase/app";
+import { IconAlertTriangle } from "@tabler/icons-react";
+
+const ERROR_CODE_INVALID_CREDENTIALS = "auth/invalid-credential";
+
+const getLoginErrorMessage = (error: unknown) => {
+  if (error instanceof FirebaseError && error.code === ERROR_CODE_INVALID_CREDENTIALS) {
+    return "Invalid email or password";
+  }
+
+  return "An uknowwn error occurred";
+};
 
 export const LoginView = () => {
   const loginMutation = useLoginMutation();
@@ -24,11 +36,16 @@ export const LoginView = () => {
     });
   };
 
+  const errorMessage = loginMutation.error ? getLoginErrorMessage(loginMutation.error) : null;
+
   return (
     <Card withBorder>
       <Title order={3} mb="1rem">
         Login
       </Title>
+      {errorMessage && (
+        <Alert variant="filled" color="danger" icon={<IconAlertTriangle />} title={errorMessage} mb="1rem" />
+      )}
       <LoginForm onSubmit={onSubmit} />
     </Card>
   );
