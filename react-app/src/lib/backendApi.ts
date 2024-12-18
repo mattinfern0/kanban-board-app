4c7dab57ebf247aa89e16f36479616a202838e8f
mattinfern0 <mattnfernandez@gmail.com>
2024-12-17 22:22:23 -0800

    FEAT: send id token with every request to backend api

--- react-app/src/lib/backendApi.ts
+++ react-app/src/lib/backendApi.ts
@@ -1,7 +1,20 @@
 import ky from "ky";
+import { firebaseAuth } from "@/features/auth/lib/firebase.ts";
 
 const backendUrl = "/api";
 
 export const client = ky.extend({
   prefixUrl: backendUrl,
+  hooks: {
+    beforeRequest: [
+      async (request) => {
+        const token = (await firebaseAuth.currentUser?.getIdToken()) || null;
+        if (token) {
+          request.headers.set("Authorization", `Bearer ${token}`);
+        } else {
+          request.headers.delete("Authorization");
+        }
+      },
+    ],
+  },
 });
