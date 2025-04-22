42a8917d46eae93b74aa07c8bcd90608bca8c7b2
mattinfern0 <mattnfernandez@gmail.com>
2025-04-22 15:32:34 -0700

    FEAT: error message for signup page

--- react-app/src/features/auth/routes/SignUpView.tsx
+++ react-app/src/features/auth/routes/SignUpView.tsx
@@ -1,8 +1,22 @@
 import { SignUpFormValues } from "@/features/auth/types";
 import { SignUpForm } from "@/features/auth/components/SignUpForm.tsx";
-import { Button, Card, Divider, Stack, Title } from "@mantine/core";
+import { Alert, Button, Card, Divider, Stack, Title } from "@mantine/core";
 import { Link, useNavigate } from "react-router";
 import { useSignUpMutation } from "@/features/auth/api/sign-up.ts";
+import { IconAlertTriangle } from "@tabler/icons-react";
+import { FirebaseError } from "firebase/app";
+
+const firebaseErrorCodeToMessage: Record<string, string> = {
+  "auth/email-already-in-use": "An account with this email already exists.",
+};
+
+const getSignUpErrorMessage = (error: unknown) => {
+  if (error instanceof FirebaseError) {
+    return firebaseErrorCodeToMessage[error.code] ?? "An uknown error occurred";
+  }
+
+  return "An uknown error occurred";
+};
 
 export const SignUpView = () => {
   const navigate = useNavigate();
@@ -21,10 +35,13 @@ export const SignUpView = () => {
     });
   };
 
+  const errorMessage: string | null = signUpMutation.error ? getSignUpErrorMessage(signUpMutation.error) : null;
+
   return (
     <Card withBorder>
       <Stack>
         <Title order={2}>Sign Up</Title>
+        {errorMessage && <Alert variant="filled" color="danger" icon={<IconAlertTriangle />} title={errorMessage} />}
         <SignUpForm onSubmit={onSubmit} />
         <Divider label="Or" />
 
